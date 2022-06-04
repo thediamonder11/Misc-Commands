6696c1fe405ce9fa78017bed916dd22652f0b3e4
thediamonder11 <76886710+thediamonder11@users.noreply.github.com>
2022-06-04 01:04:15 +0100

    base

---
+++ src/main/java/vb/$commands/PluginMain.java
@@ -0,0 +1,89 @@
+package vb.$commands;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import org.bukkit.*;
+import org.bukkit.command.*;
+import org.bukkit.event.*;
+import org.bukkit.plugin.java.*;
+
+public class PluginMain extends JavaPlugin implements Listener {
+
+	private static PluginMain instance;
+
+	@Override
+	public void onEnable() {
+		instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] commandArgs) {
+		if (command.getName().equalsIgnoreCase("die")) {
+			try {
+				((org.bukkit.entity.Damageable) (Object) commandSender).resetMaxHealth();
+				((org.bukkit.entity.Damageable) (Object) commandSender).setHealth((0d));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+		if (command.getName().equalsIgnoreCase("hello")) {
+			try {
+				commandSender.sendMessage("Hi there, Lol");
+				commandSender.sendMessage("What are you doing?");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+		return true;
+	}
+
+	public static void procedure(String procedure, List procedureArgs) throws Exception {
+	}
+
+	public static Object function(String function, List functionArgs) throws Exception {
+		return null;
+	}
+
+	public static List createList(Object obj) {
+		if (obj instanceof List) {
+			return (List) obj;
+		}
+		List list = new ArrayList<>();
+		if (obj.getClass().isArray()) {
+			int length = java.lang.reflect.Array.getLength(obj);
+			for (int i = 0; i < length; i++) {
+				list.add(java.lang.reflect.Array.get(obj, i));
+			}
+		} else if (obj instanceof Collection<?>) {
+			list.addAll((Collection<?>) obj);
+		} else if (obj instanceof Iterator) {
+			((Iterator<?>) obj).forEachRemaining(list::add);
+		} else {
+			list.add(obj);
+		}
+		return list;
+	}
+
+	public static void createResourceFile(String path) {
+		Path file = getInstance().getDataFolder().toPath().resolve(path);
+		if (Files.notExists(file)) {
+			try (InputStream inputStream = PluginMain.class.getResourceAsStream("/" + path)) {
+				Files.createDirectories(file.getParent());
+				Files.copy(inputStream, file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static PluginMain getInstance() {
+		return instance;
+	}
+}
